ee26fbd0940e77ed290afb44f8fb2338dc971b57
Abhijeet03s <abhijeetsharma476@gmail.com>
2024-09-24 01:36:15 +0530

    fixed an error regarding dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -108,7 +108,6 @@ export default function DashboardPage() {
             subscriptions={subscriptions}
             onUpdate={updateSubscription}
             onDelete={deleteSubscription}
-            onAddToCalendar={handleAddToCalendar}
          />
       </div>
    )
